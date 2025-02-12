c5c7cf6e5e572c6e889d908cb2b12e4d20d40f51
jeweler <valve.valve101@gmail.com>
2025-02-12 16:12:08 -0500

    base commit?

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// ! all imports
+import LandingPage from "@/routes/landing-page.tsx"
+
+
+const r = createBrowserRouter([
+    {
+        path: "/",
+        element: <LandingPage />,
+    }
+])
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={r} />
     </StrictMode>,
 )
---
+++ src/routes/landing-page.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+    return (
+        <>
+            <h1 className="text-3xl text-red">hello world</h1>
+        </>
+    )
+}
